675b21d9091607d336ae64e98ab295b0fd9dee77
VR-Admin <idawagenhofer9@gmail.com>
2019-02-19 10:35:49 +0100

    added SceneLoader Script

---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //required to switch between Scenes
+
+public class SceneLoader : MonoBehaviour {
+
+
+    public void PlayBtn()
+    {
+        // Load Main Level using earlier defined SceneManagement collection
+        SceneManager.LoadScene("Main");
+
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    
+
+
+    public void RetryBtn()
+    {
+        // Load Main level; is its own button in case I want to add scores later on
+        SceneManager.LoadScene("Main");
+
+    }
+
+}
+
+// Lädt die neuen Szenen, wenn Game over ist, man den 'go again' Button klickt, oder das Spiel gestartet wird.
