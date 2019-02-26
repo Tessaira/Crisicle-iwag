1496b9b544e4d211dc296731c12fd8c71dae3a76
VR-Admin <idawagenhofer9@gmail.com>
2019-02-26 10:39:33 +0100

    created timer scripts - not working yet

---
+++ Assets/MyGame/Scripts/ScoreScript.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    private Text watch;
+    private float timer;
+    private int interval = 10;
+
+	// Use this for initialization
+	void Start () 
+        watch = gameObject.GetComponent<Text>();
+        watch.text = "00:00:00";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        watch.text = string.Format("{0:00}:{1:00}:{2:00}",
+            Mathf.Floor(timer / 60),
+            Mathf.Floor(timer) % 60,
+            Mathf.Floor((timer * 100) % 100));
+
+			Debug.Log  
+       /*  if(timer > interval)
+        {
+            GlobalVariables.speed = GlobalVariables.speed + 1f;
+            interval = interval + 10;
+        } */
+}
