fa0661c0b376bca438d8fb50d56b225348ee255b
VR-Admin <idawagenhofer9@gmail.com>
2019-02-27 13:49:25 +0100

    added icicle prefab and ObjectSpawner script

---
+++ Assets/MyGame/Scripts/ObjectSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+//refs: https://stackoverflow.com/questions/11949463/how-to-get-size-of-parent-game-object
+// https://www.youtube.com/watch?v=zKG8gSrwbnQ
+// https://docs.unity3d.com/ScriptReference/Rigidbody.AddForce.html
--- Assets/MyGame/Scripts/ScoreScript.cs
+++ Assets/MyGame/Scripts/ScoreScript.cs
@@ -1,6 +1,5 @@
 ﻿using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
