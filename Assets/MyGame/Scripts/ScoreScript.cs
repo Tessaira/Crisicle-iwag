f5288e1b11f1eaec4f97c5f818e97a0a93a02a54
VR-Admin <idawagenhofer9@gmail.com>
2019-02-27 13:14:37 +0100

    timer implementation fixed

--- Assets/MyGame/Scripts/ScoreScript.cs
+++ Assets/MyGame/Scripts/ScoreScript.cs
@@ -1,20 +1,23 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
-    private Text watch;
+    private TextMeshProUGUI watch;
     private float timer;
     private int interval = 10;
 
 	// Use this for initialization
-	void Start () 
-        watch = gameObject.GetComponent<Text>();
+	void Start ()
+	{
+        watch = gameObject.GetComponent<TextMeshProUGUI>();
         watch.text = "00:00:00";
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
         timer += Time.deltaTime;
 
         watch.text = string.Format("{0:00}:{1:00}:{2:00}",
@@ -22,7 +25,7 @@ public class ScoreScript : MonoBehaviour
             Mathf.Floor(timer) % 60,
             Mathf.Floor((timer * 100) % 100));
 
-			Debug.Log  
+	}
        /*  if(timer > interval)
         {
             GlobalVariables.speed = GlobalVariables.speed + 1f;
