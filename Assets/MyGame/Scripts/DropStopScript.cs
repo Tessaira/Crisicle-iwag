ed27e3c4f1fed23eb89c5511b1afc345b23108a9
Tessaira <idawagenhofer9@gmail.com>
2019-03-19 00:59:01 +0100

    adjusted icicle colliders

---
+++ Assets/MyGame/Scripts/DropStopScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DropStopScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+
+        if (collision.CompareTag(GlobalVariables.icicle))
+        {
+            collision.GetComponentInParent<Rigidbody2D>().simulated = false;
+        }
+    }
+}
--- Assets/MyGame/Scripts/Good Scripts/Icicle.cs
+++ Assets/MyGame/Scripts/Good Scripts/Icicle.cs
@@ -2,13 +2,28 @@
 
 public class Icicle : MonoBehaviour
 {
-    private float clickAmount;
+    private int clickAmount;
 
-    [SerializeField] private float minClickAmount, maxCLickAmount;
+    [SerializeField] private GameObject dropStop;
+    [SerializeField] private int minClickAmount, maxClickAmount;
 
     private void OnMouseDown()
     {
-        clickAmount--;
+        if (clickAmount > 0)
+        {
+            clickAmount--;
+        }
+        else if (clickAmount <= 0)
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().isKinematic = false;
+            dropStop.SetActive(true);
+        }
         Debug.Log(clickAmount);
+    }
+
+    private void Start()
+    {
+        clickAmount = Random.Range(minClickAmount, maxClickAmount +1);
     }
 }
