d00d80bc344e3843ea52cda102f6b3548034e69a
Tessaira <idawagenhofer9@gmail.com>
2019-03-19 00:23:44 +0100

    adjusted icicle script

--- Assets/MyGame/Scripts/Good Scripts/Icicle.cs
+++ Assets/MyGame/Scripts/Good Scripts/Icicle.cs
@@ -2,25 +2,29 @@
 
 public class Icicle : MonoBehaviour
 {
-    private Rigidbody2D myRigidbody2D;
-    private int health = 5;
-    private string mouse1Input = "Fire1";
+    private float clickAmount;
+    private Camera cam;
 
-    private void Start ()
+    [SerializeField] private float minClickAmount, maxCLickAmount;
+
+    private void Start()
     {
-        myRigidbody2D = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
     }
 
-	void Update ()
+    private void Update()
     {
-        if (Input.GetButtonDown(mouse1Input))
-        {
-            Debug.Log ("hdioaghasdfioghiosdfgh");
-            health--;
-            if(health == 0)
-            {
-                myRigidbody2D.simulated = true;
-            }
-        }
-	}
+        Vector3 point = new Vector3();
+        Event currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+        Debug.Log()
+    }
+
+
 }
