166121ca90c47a87a77b5221fe948697f0a808ef
Tessaira <idawagenhofer9@gmail.com>
2019-03-18 17:12:08 +0100

    added movement to moving objects

--- Assets/MyGame/Scripts/Good Scripts/ObjectGod.cs
+++ Assets/MyGame/Scripts/Good Scripts/ObjectGod.cs
@@ -8,24 +8,37 @@ public class ObjectGod : MonoBehaviour
     [SerializeField] private float gap;
     [SerializeField] private GameObject icicle;
     [SerializeField] private GameObject player;
-    [SerializeField] private float yOffsetGround, xOffsetGround;
-
+    [SerializeField] private float yOffsetGround, groundSize, yOffsetIcicle;
+    [SerializeField] private ObjectMovement objectMovement;
+    
 
     private void SpawnLayer()
     {
         GameObject tempGround = Instantiate(
             ground,
-            new Vector3(player.transform.position.x + xOffsetGround + gap, player.transform.position.y - yOffsetGround, 0),
+            new Vector3(player.transform.position.x + groundSize + gap, player.transform.position.y - yOffsetGround, 0),
             Quaternion.identity);
+        objectMovement.MovingObjects.Add(tempGround);
+
+        GameObject tempIcicle = Instantiate(
+            icicle,
+            new Vector3(player.transform.position.x + groundSize / 2 + gap / 2, player.transform.position.y + yOffsetIcicle, 0),
+            Quaternion.identity);
+        objectMovement.MovingObjects.Add(tempIcicle);
     }
 
     private void Start()
     {
-        GameObject tempGround = Instantiate(
-            ground,
-            new Vector3(player.transform.position.x, player.transform.position.y - yOffsetGround, 0),
-            Quaternion.identity);
-
+        SpawnStartingObjects();
         SpawnLayer();
+    }
+
+    private void SpawnStartingObjects ()
+    {
+        GameObject tempGround = Instantiate(
+           ground,
+           new Vector3(player.transform.position.x, player.transform.position.y - yOffsetGround, 0),
+           Quaternion.identity);
+        objectMovement.MovingObjects.Add(tempGround);
     }
 }
--- Assets/MyGame/Scripts/Good Scripts/ObjectMovement.cs
+++ Assets/MyGame/Scripts/Good Scripts/ObjectMovement.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
-
+using System.Collections.Generic;
 public class ObjectMovement : MonoBehaviour
 {
-    public GameObject[] MovingObjects;
+    public List<GameObject> MovingObjects;
+
+    [SerializeField] private float objectSpeed = 0.05f;
 
     private void Update ()
     {
         foreach (GameObject singleObject in MovingObjects)
         {
-            singleObject.transform.position += new Vector3(-1 * GlobalVariables.objectMovingSpeed, 0 , 0);
+            singleObject.transform.position += new Vector3(-1 * objectSpeed, 0 , 0);
         }
     }
 }
