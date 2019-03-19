67358f92229eb1193c9719aef64e2fc47e84fd05
Tessaira <idawagenhofer9@gmail.com>
2019-03-19 20:55:53 +0100

    cleaned scripts folder, added ScriptableObject

---
+++ Assets/MyGame/Scripts/ScoreScriptable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ScoreScriptable : ScriptableObject
+{
+    public string storedTimer;
+    public string storedScore;
+}
