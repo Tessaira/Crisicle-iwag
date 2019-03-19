15216114c64183482b6e6129e8d578b5316d8403
VR-Admin <idawagenhofer9@gmail.com>
2019-03-19 10:38:26 +0100

    ScriptableObject added

---
+++ Assets/MyGame/Scripts/Good Scripts/TimerScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class TimerScriptableObject : ScriptableObject {
+
+    public string prefabName;
+
+    public int numberOfPrefabsToCreate;
+    public Vector3[] spawnPoints;
+}
